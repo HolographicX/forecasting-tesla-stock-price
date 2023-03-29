2dc9f15a69d493c4f2b736e438eea2433150ed8c
Soham Seksaria <73580740+HolographicX@users.noreply.github.com>
2023-03-29 18:21:30 +0530

    Update tesla stock forecast.py

--- tesla stock forecast.py
+++ tesla stock forecast.py
@@ -8,10 +8,20 @@ import statsmodels.api as sm
 from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.arima.model import ARIMA
 from numpy import loadtxt
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from statsmodels.graphics.tsaplots import plot_acf
+
+def difference(dataset, interval=1):
+ diff = list()
+ for i in range(interval, len(dataset)):
+    value = dataset[i] - dataset[i - interval]
+    diff.append(value)
+ return diff
 
 fig, ax = plt.subplots()
-ax.set_xlabel('time')
-ax.set_ylabel('amplitude')
+ax.set_xlabel('time (days from 2016-02-12)')
+ax.set_ylabel('closing price')
 plt.title("Tesla stock prices 2016-2020")
 
 time = np.arange(1, 1000, 1)
@@ -19,55 +29,57 @@ time = np.arange(1, 1000, 1)
 # tesla stock prices from 
 file = open('/run/media/soham/4024229A242292C8/Main/python/tcs internship/time series/tesla stock forecast/tesla-stock-2016-2020.csv', 'rb')
 y = loadtxt(file,delimiter = ",")
-print(y)
+# print(y)
 
 # plot_pacf(y, lags=39, title=" Autocorrelation - Non-Stationary process 1")
 # plt.scatter(time, y)
 # plt.show()
+
 
 '''
 -------------
 --- ARIMA ---
 ------------- 
 '''
-y_arima = ARIMA(y, order=(1,0,0)).fit()
+'''
+---
+'''
+plot_acf(y, lags=999, title="Autocorrelation Function (ACF) - Tesla Stock Prices (2016-2020)")
+plot_acf(difference(y), lags=998, title="Autocorrelation Function (ACF) - diff(Tesla Stock Prices (2016-2020))")
 
-x_start = 500;
+plt.show()
+data = y
 
-x_end = 1000;
+train_size = int(len(data) * 0.66)
+train, test = data[0:train_size], data[train_size:len(data)]
+history = [x for x in train]
+predictions = list()
+print(data[train_size-10:train_size+3])
 
-y_hat = y_arima.predict(x_start,x_end)
+for t in range(len(test)):    
+    model = ARIMA(history, order=(2,1,2))
+    model_fit = model.fit()
+    
+    pred = model_fit.forecast()
+    yhat = pred[0]
+    predictions.append(yhat)    # Append test observation into overall record
+    obs = test[t]
+    history.append(obs)
+    if t < 6: 
+        print(model_fit.summary())
+        print(t, " prediction vs actual ", list(data).index(obs) , str(pred), str (obs) )
 
-plt.plot(y, linewidth = 1, label = "Actual")
-
-plt.plot(list(range(x_start, x_end+1)), y_hat, color= "blue",linestyle="dotted", linewidth = 2, label = "Predicted with (1, 0, 0)")
-
+plt.plot(data, color= "grey", linewidth = 2, label = "Actual")
+plt.plot( list(range(train_size, len(data))), predictions, linestyle="dotted", linewidth = 3, label = "Predicted with (2,1,2)")
+#list(range(train_size, len(data))),
 plt.legend()
 
 plt.show()
-# train = y[0:800]
-# test = y[-199:]
-# # plt.plot(train)
-# plt.plot(test)
-# plt.plot(train)
-# plt.show()
-# model = ARIMA(train, order = (1, 1, 1))
-  
-# result = model.fit()
-# print(result.summary())
-
-# timeplot = time[0:800]
-# yplot = y[0:800]
-# plt.plot(timeplot, yplot)
-# plt.plot(result.predict(10,80), color='red')
-
-# y0 = y[1:999]
-# y1 = y[0:998]
-# z = []
-# for i in range(len(y0)):
-#     z.append(y0[i] - y1[i])
-    
+rmse = sqrt(mean_squared_error(test, predictions))
+print('Test RMSE: %.3f' % rmse)
+print("max: " + str(max(y)))
+print("min: " + str(min(y)))
 
 
-# plt.plot(z)
-# plt.show()
+
+
